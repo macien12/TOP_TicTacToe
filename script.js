c83c2476c71e47f91377c3f3e753faa5a6facab0
macien12 <160124196+macien12@users.noreply.github.com>
2026-07-28 19:46:46 +0200

    Added new function to make the game work in console

--- script.js
+++ script.js
@@ -26,6 +26,12 @@ const GameController = (function() {
 
     let currentPlayer = player1;
 
+    const WINNING_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+
     function switchTurn() {
         if (currentPlayer === player1) {
             currentPlayer = player2;
@@ -34,11 +40,32 @@ const GameController = (function() {
         }
     }
 
-    function playRound(index) {
-        Gameboard.placeMark(index, currentPlayer.mark);
-        switchTurn();
+    function checkWin() {
+        const board = Gameboard.getBoard();
+        return WINNING_COMBINATIONS.some(combination => {
+            return combination.every(index => board[index] === currentPlayer.mark);
+        });
+    };
+
+    function checkTie() {
+        const board = Gameboard.getBoard();
+        return !board.includes("");
     }
 
+    function playRound(index) {
+        if (Gameboard.getBoard()[index] === ""){
+            Gameboard.placeMark(index, currentPlayer.mark);
+            
+            if (checkWin()) {
+                console.log(`${currentPlayer.mark} wins!`)
+            } else if (checkTie()) {
+                console.log("It's a tie!");
+            } else {
+                switchTurn();
+            }
+        }
+    }
+    
     return {
         playRound,
     };
